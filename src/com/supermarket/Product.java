08249506f325622606a8240e199bec32f5d805cf
Abhinav saini <abhinavsaini632@gmail.com>
2025-12-18 21:03:28 +0530

    Product.java

---
+++ src/com/supermarket/Product.java
@@ -0,0 +1,19 @@
+package com.supermarket;
+
+public class Product {
+    int id;
+    String name;
+    double price;
+    int quantity;
+
+    Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    double getTotalPrice() {
+        return price * quantity;
+    }
+}
